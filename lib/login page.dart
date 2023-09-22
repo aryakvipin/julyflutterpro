f7a3ea76b1e62709cbeb3ebefebfb8b2e5a94262
aryakvipin <arya28985@gmail.com>
2023-09-22 11:27:16 +0530

    commit

--- lib/login page.dart
+++ lib/login page.dart
@@ -4,15 +4,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:julyflutterpro/registpage.dart';
 
-
-
 class Loginpage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => LoginState();
 }
 
 class LoginState extends State<Loginpage> {
-    bool showpass =true;
+  bool showpass = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,10 +36,10 @@ class LoginState extends State<Loginpage> {
                 height: 50,
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 50,right: 50),
+                padding: const EdgeInsets.only(left: 50, right: 50),
                 child: TextField(
                   decoration: InputDecoration(
-                    hintText: "username",
+                      hintText: "username",
                       prefixIcon: Icon(Icons.person),
                       labelText: "username",
                       border: OutlineInputBorder(
@@ -48,43 +47,54 @@ class LoginState extends State<Loginpage> {
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 50,right: 50,top: 50),
+                padding: const EdgeInsets.only(left: 50, right: 50, top: 50),
                 child: TextField(
-                   obscureText: showpass,
+                  obscureText: showpass,
                   obscuringCharacter: "*",
                   decoration: InputDecoration(
                       hintText: "password",
-
                       prefixIcon: Icon(Icons.person),
-                      suffixIcon:IconButton(onPressed: () {
-                           setState(() {
-                             if(showpass){
-                               showpass=false;
-                             }
-                             else
-                               {
-                                 showpass=true;
-                               }
-
-                           });
-
-                      }, icon: Icon(showpass==true ? Icons.visibility_off : Icons.visibility),),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            if (showpass) {
+                              showpass = false;
+                            } else {
+                              showpass = true;
+                            }
+                          });
+                        },
+                        icon: Icon(showpass == true
+                            ? Icons.visibility_off
+                            : Icons.visibility),
+                      ),
                       labelText: "password",
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(20))),
                 ),
               ),
-               SizedBox(height: 50,),
-              SizedBox(width: 80
-                ,
+              SizedBox(
                 height: 50,
-                child: ElevatedButton(style: ElevatedButton.styleFrom(backgroundColor: Colors.deepOrange, side: BorderSide(width: 1.0,color: Colors.cyan) ),
-                    onPressed: (){}, child: Text(" Login ")),
               ),
-              SizedBox(height: 50,),
-              TextButton(onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>Register()));
-              }, child: Text("Not a user ? create a new account")),
+              SizedBox(
+                width: 80,
+                height: 50,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.deepOrange,
+                        side: BorderSide(width: 1.0, color: Colors.cyan)),
+                    onPressed: () {},
+                    child: Text(" Login ")),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              TextButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Register()));
+                  },
+                  child: Text("Not a user ? create a new account")),
             ],
           ),
         ),
